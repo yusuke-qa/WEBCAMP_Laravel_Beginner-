3c798444ff813512093dcdd268d346c74767d27f
yusuke <shindo1320366@gmail.com>
2026-05-30 13:18:43 +0000

    サンプルページのURLを変更した

--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 
-Route::get('/', [WelcomeController::class, 'index']);
+Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
